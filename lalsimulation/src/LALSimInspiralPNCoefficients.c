60b2f4e8fef8c82b8a542fc137635550361eb880
Reetika Dudi <spxrd@raven14.bullx>
2017-12-12 20:04:38 +0000

    added new phase terms

--- lalsimulation/src/LALSimInspiralPNCoefficients.c
+++ lalsimulation/src/LALSimInspiralPNCoefficients.c
@@ -658,6 +658,70 @@ XLALSimInspiralPNPhasing_F2(
     pfa->v[7] = LAL_PI * ( 77096675.L/254016.L
                      + 378515.L/1512.L * eta - 74045.L/756.L * eta*eta);
 
+    pfa->v[8] = c_21_3PN*(40.L/81.L * eta - 160.L/81.L * eta * eta)
+                + c_22_4PN * 160.L/9.L
+                - 369469478275.L/16019011008.L * eta * eta * eta * eta 
+                + 510041481025.L/13106463552.L * eta * eta * eta
+                + (300600673165997.L/576684396288.L 
+                - 399545.L/27216.L * LAL_PI * LAL_PI) * eta * eta
+                + (-5679872289503527.L/1281520880640.L - 5322928.L/3969.L * LAL_GAMMA 
+                + 9302215.L/54432.L * LAL_PI * LAL_PI - 1420688.L/441.L * log(2.)
+                + 26325.L/ 49.L * log(3.)) * eta - 90490.L/567.L * LAL_PI * LAL_PI
+                 - 36812.L/189.L * LAL_GAMMA + 2550713843998885153.L/830425530654720.L
+                 -26325.L/196.L * log(3.) - 1011020.L/3969.L * log(2.);
+    pfa->vlogv[8] = -3 *(c_21_3PN*(40.L/81.L * eta - 160.L/81.L * eta * eta)
+                 + c_22_4PN * 160.L/9.L
+                 - 369469478275.L/16019011008.L * eta * eta * eta * eta
+                 + 510041481025.L/13106463552.L * eta * eta * eta
+                 + (300600673165997.L/576684396288.L 
+                 - 399545.L/27216.L * LAL_PI * LAL_PI) * eta * eta
+                 + (-5679872289503527.L/1281520880640.L - 5322928.L/3969.L * LAL_GAMMA
+                 + 9302215.L/54432.L * LAL_PI * LAL_PI - 1420688.L/441.L * log(2.)
+                 + 26325.L/ 49.L * log(3.)) * eta - 90490.L/567.L * LAL_PI * LAL_PI
+                  - 36812.L/189.L * LAL_GAMMA + 2550713843998885153.L/830425530654720.L
+                  -26325.L/196.L * log(3.) - 1011020.L/3969.L * log(2.));
+    pfa->vlogvlogv[8] = (2661464.L/11907.L * eta + 18406.L/567.L );
+    pfa->v[9] = LAL_PI*(10323755.L/199584.L * eta * eta * eta 
+                 + 45293335.L/127008.L * eta * eta
+                 + (2255.L/6.L * LAL_PI * LAL*PI - 1492917260735.L/134120448.L)* eta
+                 -640.L/3.L * LAL_PI * LAL_PI - 13696.L/21.L * LAL_GAMMA 
+                 + 105344279473163.L/18776862720.L - 27392.L/21.L * log(2.));
+    pfa->vlogv[9] =  - 13696.L/63.L *LAL_PI;
+    pfa->v[10] = 1.L/(1.L - 3.L * eta) * (a_6_c * (72.L*eta - 216.L*eta*eta) 
+                 + c_21_3PN *(-764.L/21.L * eta * eta * eta * eta 
+                 - 599.L/63.L * eta * eta * eta + 2815.L/189.L * eta * eta - 484.L/189.L * eta) 
+                 + c_22_ 4PN *( 2564.L/7.L *eta *eta *eta - 698.L/21.L * eta * eta 
+                 - 622.L/21.L * eta) + c_22_5PN *(48.L*eta*eta -16.L*eta )
+                + (2425066585102052979797.L/857232704816167680.L)*eta *eta*eta*eta*eta*eta
+                - (12721434740371951621.L/676317715831296.L)*eta *eta *eta *eta *eta
+                + (11160810800663155149913.L/272137366608307200.L
+                 -9434797.L/18816.L*LAL_PI*LAL_PI) *eta *eta *eta *eta 
+                + (-857104076559310860540851.L/34289308192646707200.L
+                -6147793142.L/1528065.L*LAL_GAMMA - 460519.L/1536.L * LAL_PI*LA_PI
+                -43111797668.L/1528065.L*log(2.) + 1279395.L/98.L*log(3.)) *eta *eta *eta
+                + (-18736399363805057301105217.L/365752620721564876800.L  
+                - (99239192119.L/4584195.L)*LAL_GAMMA + 415795517.L/903168.L * LAL_PI * LAL_PI
+                - 117340379713.L/4584195.L*log(2.) - 58330935.L/5488.L*log(3.))*eta*eta
+                + (569935181259668744781113.L/10837114688046366720.L
+                + 63787407527.L/9168390.L * LAL_GAMMA - 5451429547.L/8128512.L * LAL_PI * LAL_P
+                + 159943397077.L/18336780.L * log(2.)+ (8924175.L/3136.L)*log(3.)) * eta 
+                + (578223115.L/3048192.L)*LAL_PI*LAL_PI+ (6470582647.L/27505170.L)*LAL_GAMMA 
+                - 1433006523295407126559.L/126306697995878400.L - 5512455.L/21952.L*log(3.)
+                + 53992839431.L/55010340.L*log(2.));
+    pfa->vlogv[10] = 1.L/(1.L - 3.L * eta) * (12863780362.L/4584195.L * eta * eta * eta
+                + 13849493129.L/13752585.L * eta * eta
+                - 24279431641.L/27505170.L * eta+ 6470582647.L/82515510.L);
+    pfa->v[11] = LAL_PI*( c_21_3PN * (-160.L/27.L * eta * eta + 40.L/27.L * eta);
+                + 320.L/9.L * eta * c_22_4PN + 657627073445.L/144171099072.L *eta *eta *eta *eta 
+                - 1080597828475.L/26212927104.L *eta *eta *eta 
+                + 5120314955146397.L/629110250496.L *eta*eta
+                +  (-10647905.L/36288.L *eta *eta + 45015785.L/145152.L * eta
+                - 94390.L/567.L) *LAL_PI + (-1346662.L/567.L * LAL_GAMMA 
+                - 430383707398397047.L/34601063777280.L + 6325.L/49.L * log(3.) 
+                - 21009626.L/3969.L * log(2.)) *eta - 3558011.L/7938.L *LAL_GAMMA
+                + 1857541407236594411.L/276808510218240.L
+                - 26325.L/196.L * log(3.) - 862549.L/1134.L * log(2.));
+    pfa->vlogv[11] = -LAL_PI * (1346662.L/1701.L * eta + 3558011.L/23814.L);
     /* modify the PN coefficients if a non null LALSimInspiralTestGRParam structure is passed */
     pfa->v[0]*=(1.0+XLALSimInspiralWaveformParamsLookupNonGRDChi0(p));
     pfa->v[1] = XLALSimInspiralWaveformParamsLookupNonGRDChi1(p);
